d3a9ff286f950aeb37d40653abae5e93bbf26767
RedRe4per <s3582474@gmail.com>
2022-10-02 01:34:58 +1000

    change

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import axios from '../../api/demoApi';
-import { nanoid } from 'nanoid'
 import { useState, useEffect } from 'react';
+import { useStripe } from '@stripe/react-stripe-js';
 
 const P = styled.p`
     font-weight: bold;
@@ -14,9 +14,12 @@ const TableHeader = styled.div`
     background-color: lightgray;
     font-weight: bold;
     font-size: large;
+    padding: 0px 20px 0px 40px;
 `;
 
-const TableContent = styled.div``;
+const TableContent = styled.div`
+    padding: 0px 20px 0px 40px;
+`;
 
 const Item = styled.div`
     display: flex;
@@ -31,11 +34,19 @@ const Img = styled.img`
 `;
 
 const Div = styled.div`
+    flex: 4;
+`;
+
+const DivCheckbox = styled.div`
     flex: 1;
 `;
 
+const DivQuotationDetails = styled.div`
+    flex: 8;
+`;
+
 const FooterDiv = styled.div`
-    margin-top: 40px;
+    margin-top: 50px;
 `;
 
 const Button = styled.a`
@@ -51,46 +62,102 @@ const Button = styled.a`
 `;
 
 const ShoppingCart = () => {
-    const [cartItems, setCartItems] = useState([]);
+    const [cartItemsRebuilt, setCartItemsRebuilt] = useState([]);
+    const stripe = useStripe();
 
     useEffect(() => {
         axios({
             method: 'GET',
             url: '/cartItem',
         }).then(response => {
-            setCartItems(response.data);
+            const newArr = response.data.map((cartItem) => {
+                let quotationDetailsStr = "";
+                cartItem.quotationDetails.forEach((element) => {
+                    quotationDetailsStr += ` Color: ${element.color}, Quantity: ${element.quantity};`;
+                })
+                cartItem.quotationDetails = quotationDetailsStr;
+                cartItem.checked = false;
+                return cartItem;
+            })
+            setCartItemsRebuilt(newArr);
+
         }).catch(err => {
             alert("error", err)
         });
     }, [])
 
+    const checkboxHandler = (id) => {
+        const newCartItems = cartItemsRebuilt.map((cartItem)=>{
+            if(cartItem._id === id) cartItem.checked = !cartItem.checked;
+            return cartItem;
+        })
+        setCartItemsRebuilt(newCartItems)
+    };
+
+    const CheckoutHandler = async () => {
+        const line_items = cartItemsRebuilt.map((cartItem) => {
+            return {
+                quantity: 1,
+                price_data: {
+                    currency: "aud",
+                    unit_amount: 100,
+                    product_data: {
+                        name: cartItem.design.designName,
+                        description: cartItem.quotationDetails,
+                        images: [cartItem.image],
+                    }
+                }
+            }
+        })
+        const dataObj = {line_items: line_items, customer_email: "blhxp1@gmail.com"}
+        
+        axios({
+            method: 'POST',
+            url: '/payment/create-checkout-session',
+            data: dataObj
+        }).then(response => {
+            const { sessionId } = response.data;
+            stripe.redirectToCheckout({
+                sessionId
+            })
+
+        }).catch(err => {
+            console.log(err)
+        });
+
+    }
+
     return (
         <section>
             <P>CART</P>
             <TableHeader>
+                <DivCheckbox>Select</DivCheckbox>
                 <Div>Image</Div>
                 <Div>Product</Div>
                 <Div>Quotation</Div>
-                <Div>Quotation Details</Div>
+                <DivQuotationDetails>Quotation Details</DivQuotationDetails>
             </TableHeader>
             <TableContent>
                 {
-                    cartItems.map((cartItem) => {
-                        return(
-                        <Item key={nanoid()}>
-                            <Div>
-                                <Img src={cartItem.image} alt="no pic"></Img>
-                            </Div>
-                            <Div>{cartItem.design.designName}</Div>
-                            <Div>{cartItem.quotation}</Div>
-                            <Div>{cartItem.quotation}</Div>
-                        </Item>
+                    cartItemsRebuilt.map((cartItem) => {
+                        return (
+                            <Item key={cartItem._id}>
+                                <DivCheckbox>
+                                    <input type="checkbox" checked={cartItem.checked} onChange={()=> checkboxHandler(cartItem._id)}></input>
+                                </DivCheckbox>
+                                <Div>
+                                    <Img src={cartItem.image} alt="no pic"></Img>
+                                </Div>
+                                <Div>{cartItem.design.designName}</Div>
+                                <Div>{cartItem.quotation}</Div>
+                                <DivQuotationDetails>{cartItem.quotationDetails}</DivQuotationDetails>
+                            </Item>
                         )
                     })
                 }
             </TableContent>
             <FooterDiv>
-                <Button>Proceed to Checkout</Button>
+                <Button onClick={CheckoutHandler}>Proceed to Checkout</Button>
             </FooterDiv>
         </section>
     )
